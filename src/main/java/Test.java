7d3d9f8cabf9854fab9c02a5bd25cc754dbdf61e
crin <crinuyi@users.noreply.github.com>
2019-03-28 22:42:34 +0100

    created FileManager with sample class
    (to check the functionality)

---
+++ src/main/java/Test.java
@@ -0,0 +1,29 @@
+public class Test {
+    private int id;
+    private String name;
+
+    public Test() {}
+
+    public Test(int id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
